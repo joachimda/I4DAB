3ffa26dca196b1e07e1115fddc2668e5e4d8ac7b
joachimda <joachimdamandersen@gmail.com>
2016-05-29 23:36:27 +0200

    added override method in dbcontext

--- LAB 1/CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/BloggingContext.cs
+++ LAB 1/CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/BloggingContext.cs
@@ -7,5 +7,13 @@ namespace CodeFirstNewDatabaseSample
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
     }
 }
