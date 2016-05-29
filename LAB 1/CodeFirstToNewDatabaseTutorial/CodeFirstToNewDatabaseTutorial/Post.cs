8f9fab8c75a5f718c84c38da5a2128b455123fd7
joachimda <joachimdamandersen@gmail.com>
2016-05-29 22:16:04 +0200

    added basics in tutorial

---
+++ LAB 1/CodeFirstToNewDatabaseTutorial/CodeFirstToNewDatabaseTutorial/Post.cs
@@ -0,0 +1,11 @@
+namespace CodeFirstToNewDatabaseTutorial
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
